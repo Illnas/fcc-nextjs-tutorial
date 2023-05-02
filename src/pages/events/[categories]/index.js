236b3db0d2ba3967c4ab4c9b090c89d1f6031870
Dario Zalac <dariosting@gmail.com>
2023-05-02 22:13:28 +0200

    mapping overr event routes done

--- src/pages/events/[categories]/index.js
+++ src/pages/events/[categories]/index.js
@@ -1,16 +1,44 @@
-const EventsPerCityPage = () => {
-    return (
+const EventsPerCityPage = ({data}) => {
+  return (
+    <div>
+      <h1>Events in London</h1>
       <div>
-        <h1>Our Single Event Page Index</h1>
-
-        <a href="/events/event1">Event 1</a>
-        <a href="/events/event2">Event 2</a>
-        <a href="/events/event3">Event 3</a>
-        <a href="/events/event4">Event 4</a>
-        <a href="/events/event5">Event 5</a>
-        <a href="/events/event6">Event 6</a>
+        {data.map(ev => (
+          <a key={ev.id} href={`/events/${ev.city}/${ev.id}`}>
+            <img src={ev.image} alt={ev.title} />
+            <h2>{ev.title}</h2>
+            <p>{ev.description}</p>
+          </a>
+        ))}
       </div>
-    );
+    </div>
+  );
+};
+
+export default EventsPerCityPage;
+
+export async function getStaticPaths() {
+  const { events_categories } = await import('/data/data.json');
+  const allPaths = events_categories.map((ev) => {
+    return {
+      params: {
+        categories: ev.id.toString(),
+      },
+    };
+  });
+  console.log(allPaths);
+  return {
+    paths: allPaths,
+    fallback: false,
   };
-  
-  export default EventsPerCityPage;
+}
+
+export async function getStaticProps(context) {
+  console.log(context);
+  const id = context?.params.categories;
+  const { allEvents } = await import('/data/data.json');
+
+  const data = allEvents.filter((ev) => ev.city === id);
+
+  return { props: { data, pageName: id } };
+}
